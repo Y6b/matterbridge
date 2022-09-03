a63bb8897f654c4c902ca5ada5b01946fade6c38
Bushl <butti@u>
2022-09-03 19:13:34 +0400

    Whatsapp sticker message handling - fixing sticker download link to mmg - bug fix

--- bridge/whatsappmulti/handlers.go
+++ bridge/whatsappmulti/handlers.go
@@ -284,7 +284,7 @@ func (b *Bwhatsapp) handleStickerMessage(msg *events.Message) {
 	b.Log.Infof("Receiving sticker message %+v", imsg)
 
     //set URL to mmg.whatsapp.net instead of web.whatsapp.net
-    imsg.url := "https://mmg.whatsapp.net"
+    imsg.Url := "https://mmg.whatsapp.net"
 
 	senderJID := msg.Info.Sender
 	senderName := b.getSenderName(senderJID)

a56e7d00ba9948560b832dc1b09be324795dde6f
Bushl <butti@u>
2022-09-06 23:37:27 +0400

    Whatsapp sticker message handling from gateway to Whatsapp - animated switch - fix

--- bridge/whatsappmulti/whatsapp.go
+++ bridge/whatsappmulti/whatsapp.go
@@ -383,7 +383,7 @@ func (b *Bwhatsapp) PostStickerMessage(msg config.Message, filetype string) (str
 		FileLength:    goproto.Uint64(resp.FileLength),
 		Url:           &resp.URL,
 		DirectPath:    &resp.DirectPath,
-		isAnimated:     true,
+		IsAnimated:     true,
 	}
 
 	b.Log.Debugf("=> Sending %#v", msg)

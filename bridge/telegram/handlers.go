f7bb4e8f91306fd58fb8c26fe298e3405300d0b7
Bushl <butti@u>
2022-09-10 22:50:43 +0400

    Vcard Whatsapp to Telegram implementation - test 6 - fix 7 - debug 2

--- bridge/telegram/handlers.go
+++ bridge/telegram/handlers.go
@@ -422,7 +422,7 @@ func (b *Btelegram) handleContactMessage(msg *config.Message, chatid int64) (str
 
 	cfg := tgbotapi.NewContact(chatid, "9999","test")
 	cfg.VCard = msg.Text
-
+    b.Log.Debugf("Contact message made: %+v",cfg)
 
 	res, err := b.c.Send(cfg)
 
